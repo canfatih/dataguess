7b2eec6aae3fe6f586786f35d20cf3063a190a04
canfatih <fatihcan@marun.edu.tr>
2023-09-12 22:56:32 +0300

    simple filter added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Apollo from "./components/Apollo"
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
-import Filterlist from './components/Filterlist';
+
 import DisplayCountry from './components/DisplayCountry';
 function App() {
   return (
     <div className="App">
       <h1 style={{textAlign:"center"}}>Filter İtems</h1>
-      <Filterlist></Filterlist>
+     
       <DisplayCountry></DisplayCountry>
     </div>
   );
--- src/components/DisplayCountry.tsx
+++ src/components/DisplayCountry.tsx
@@ -1,35 +1,57 @@
-import React from 'react'
+import React, { useRef,ChangeEvent, useEffect, useState } from 'react'
 import {useQuery, gql} from '@apollo/client';
 import Listitem from './Listitem';
 import "./DisplayCountry.css"
 export const DisplayCountry = () => {
-   
+  const inputRef=useRef<HTMLInputElement>(null);
+  const [countryList,setcountryList]=useState<IGET_COUNTRIES[]|undefined>()
+  const[searchTerm,setsearchTerm]=useState<string|undefined>("")
+   let deneme:string="BR"
     const GET_COUNTRIES = gql`
-  query GetCountries {
-    countries {
-      name
-     
-      
-    }
-  }
-`;
+    query getcountry{
+      countries{
+        name
+        code
+      }
+    }`
+    const get_codes = gql`
+    query getcountry{
+      country(code:${deneme}){
+        name
+      }
+    }`
 
 const { loading, error, data } = useQuery(GET_COUNTRIES);
 
 interface IGET_COUNTRIES{
 name:string
-__typename:string
-   
-    
-    
 }
-if (loading) return <p>Loading...</p>;
-if (error) return <p>Error : {error.message}</p>;
 
-const countryData:IGET_COUNTRIES[]= data.countries;
+
+
+const handleChange=()=>{
+
+  const current=inputRef.current?.value
+setsearchTerm(current)
+}
+
+useEffect(()=>{
+ 
+  let countryData:IGET_COUNTRIES[]= data?.countries
+if(countryData&&searchTerm){
+  
+  countryData=countryData.filter((item)=>item.name.includes(searchTerm))
+
+
+}
+setcountryList(countryData)
+},[data,searchTerm])
+// if (loading) return <p>Loading...</p>;
+// if (error) return <p>Error : {error.message}</p>;
   return (
     <div  className="countries">
-    {countryData.map((country,index)=>(
+      <input onChange={handleChange} ref={inputRef} className="filterlist" />
+    {countryList?.map((country,index)=>(
         <Listitem key={index} name={country.name}></Listitem>
     ))}
         
